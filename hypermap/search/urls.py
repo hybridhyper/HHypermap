dcdaa87be85f57853cb1211e28319a2295f1e5de
Tom Kralidis <tomkralidis@gmail.com>
2016-06-24 18:19:38 -0400

    tidy up urlpatterns, move catalog slug under /csw

--- hypermap/search/urls.py
+++ hypermap/search/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls import include, patterns, url
 from . import views
 
 urlpatterns = [
-    url(r'^search/docs/', include('rest_framework_swagger.urls')),
     url(r'^search/?$', views.search_api, name='search_api'),
-    url(r'^search/csw/(?P<catalog_slug>[\w-]+)/$', views.csw_global_dispatch_by_catalog,
-        name='csw_global_dispatch_by_catalog'),
+    url(r'^search/docs/', include('rest_framework_swagger.urls')),
     url(r'^search/csw$', views.csw_global_dispatch, name='csw_global_dispatch'),
+    url(r'^search/(?P<catalog_slug>[\w-]+)/csw/$', views.csw_global_dispatch_by_catalog,
+        name='csw_global_dispatch_by_catalog'),
     url(r'^search/opensearch$', views.opensearch_dispatch, name='opensearch_dispatch')
 ]
 
